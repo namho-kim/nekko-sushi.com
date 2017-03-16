c047c436698b59ab98ca163eb1e230d826258fb4
Namho Kim <nk@namho.kim>
2017-03-16 11:57:19 -0700

    nekko sushi website

---
+++ html/menu/javascript.js
@@ -0,0 +1,19 @@
+function lunch() {
+    document.getElementById("imageBox").innerHTML = '<img class="images" src="css/images/lunch.jpg">'
+};
+
+function apps() {
+    document.getElementById("imageBox").innerHTML = '<img class="images" src="css/images/apps.jpg"><img class="images" src="css/images/salads.jpg">'
+};
+
+function dinner() {
+    document.getElementById("imageBox").innerHTML = '<img class="images" src="css/images/dinner.jpg"><img class="images" src="css/images/combo.jpg">'
+};
+
+function rolls() {
+    document.getElementById("imageBox").innerHTML = '<img class="images" src="css/images/rolls1.jpg"><img class="images" src="css/images/rolls2.jpg">'
+};
+
+function beverage() {
+    document.getElementById("imageBox").innerHTML = '<img class="images" src="css/images/drinks.jpg">'
+};
